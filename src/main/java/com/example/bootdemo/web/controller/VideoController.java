3e6f6ffdbe2bba1fdfdd22d28e2dc86298239744
hanyang <342401744@qq.com>
2022-12-13 17:05:23 +0800

    first commit

---
+++ src/main/java/com/example/bootdemo/web/controller/VideoController.java
@@ -0,0 +1,24 @@
+package com.example.bootdemo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author: YANG
+ * Date: 2022/7/1 11:33
+ * Describe:
+ */
+@Controller
+public class VideoController {
+
+    @RequestMapping(value="/video/index")
+    public String index(){
+        System.out.println("viedoindex");
+        return "page/index";
+    }
+    @RequestMapping(value="/wvideo/index")
+    public String wvideoIndex(){
+        System.out.println("wvideo");
+        return "page/windex";
+    }
+}
